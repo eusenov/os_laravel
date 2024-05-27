1d87ea5a0bba27ae9de9cf5c926687dcb498c756
eusenov <eusenov961@gmail.com>
2024-05-27 19:09:45 +0300

    before editing registration/authorization

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,10 +22,22 @@
     </header>
     <div>
         <div class="regBlock wrapper">
-            <div class="regBlock__content">
-                <a href="{{ url('/login') }}">Войти</a>
-                <a href="{{ url('/registration') }}">Зарегистрироваться</a>
-            </div>
+        <nav>
+            <ul>
+                @guest
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                    <li><a href="{{ route('logout') }}" 
+                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Logout
+                    </a></li>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                @endguest
+            </ul>
+        </nav>
         </div>
         {{ $content }}
     </div>
